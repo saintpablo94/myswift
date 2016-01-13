16f9f2db50a6cc0addf15b12c34c1f43e02c3e53
saintpablo94 <saint.pablo94@gmail.com>
2016-01-13 21:15:08 +0900

    swift 134p

---
+++ chapter01/MyFirstSwift/MyFirstSwift/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MyFirstSwift
+//
+//  Created by NHNENT on 2016. 1. 13..
+//  Copyright © 2016년 NHNENT. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    let year = 1996
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+        
+}
+
+
---
+++ chapter01/MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var year : Int
+year = 1996
+
+let YEAR : Int
+YEAR = 2000
+
+Int.max
+Int.min
+
+Int64.max
+Int64.min
+
+INT8_MAX
+INT8_MIN
+
+UINT8_MAX
+
+var temp_1 = 3
+
+var temp_2 : Float = 3
+
+
