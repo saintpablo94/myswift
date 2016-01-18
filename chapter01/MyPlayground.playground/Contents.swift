f8ea9399d1351139840236aac126a0e34fd4c561
saintpablo94 <saint.pablo94@gmail.com>
2016-01-18 20:08:51 +0900

    swift 166p

--- chapter01/MyPlayground.playground/Contents.swift
+++ chapter01/MyPlayground.playground/Contents.swift
@@ -26,3 +26,63 @@ var temp_1 = 3
 var temp_2 : Float = 3
 
 
+let name = "마스터"
+let birth = 80
+
+let profile = "\(name)님은 \(birth) 탄생하심"
+let desc = " \(birth+40) 결과는"
+print(profile)
+
+let a = 10
+let b = 15
+
+-a
+-b
+
+a+b
+a-b
+a*b
+a/b
+a%b
+
+print(1...5)
+print(1..<5)
+
+let aa = 1
+let bb = 5
+
+for row in aa...bb{
+    row
+    
+}
+
+for var i = 1 ; i < 10 ; i++ {
+    print("\(i) 번째 ")
+}
+
+var lang = "swift"
+for char in lang.characters {
+    print("\(char) 입니다.")
+}
+
+var n = 1024
+while n < 10 {
+    n += 2
+}
+
+while n < 1000 {
+    n *= 2
+}
+
+print(n)
+
+repeat {
+    n *= 2
+}
+while n < 1000
+
+print(n)
+
+
+
+
